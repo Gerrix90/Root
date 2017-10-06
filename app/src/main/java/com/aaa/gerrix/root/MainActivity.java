9f691e4872cc92f086e0cfa966054a1691ade368
Gerrix <jahja.trifunovic@gmail.com>
2017-10-06 13:58:35 +0200

    ButterKnife aplied

--- app/src/main/java/com/aaa/gerrix/root/MainActivity.java
+++ app/src/main/java/com/aaa/gerrix/root/MainActivity.java
@@ -3,35 +3,51 @@ package com.aaa.gerrix.root;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.EOFException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.concurrent.TimeoutException;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Provera";
-    private TextView devicesTv;
+    private RootManagerHelper packetMennager;
+    @BindView(R.id.devicesTV) TextView devicesTv;
+    @BindView(R.id.loadClients) Button loadClients;
+    @BindView(R.id.clearTV) Button clearTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        ButterKnife.bind(this);
 
-        devicesTv = (TextView) findViewById(R.id.devicesTV);
 
-        RootManagerHelper packetMennager = new RootManagerHelper();
+         packetMennager = new RootManagerHelper();
 
-        devicesTv.setText(packetMennager.scanWlan().getMessage());
 
+    }
+
+    @OnClick({R.id.loadClients, R.id.clearTV})
+    public void handleClickEvent(View view){
+
+        switch (view.getId()){
+
+            case R.id.loadClients:
+                devicesTv.setText(packetMennager.scanWlan().getMessage());
+                break;
+
+            case R.id.clearTV:
+                if (!devicesTv.getText().toString().isEmpty())
+                    devicesTv.setText("");
+                break;
+
+        }
     }
 }
 
